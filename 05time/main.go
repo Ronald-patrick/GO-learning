8c0faf878b0d3f86b33ccfd367ee19b35f9c730e
Ronald-patrick <ronald.patrick1101@gmail.com>
2022-11-22 08:55:38 +0530

    time module

--- 05time/main.go
+++ 05time/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Welcome to Conversions")
+	fmt.Println("Welcome to Time Module")
 
-	var 
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	fmt.Println(time.Date(2020, time.December, 11, 22, 23, 0, 0, time.Local))
 }
+
+// Build with go build GOOS="windows" go build
