c83a8b87637b273808a9a08a2d548481f59024ba
Ronald-patrick <ronald.patrick1101@gmail.com>
2022-11-22 09:39:38 +0530

    Added more examples to Module - 6

--- 06pointers/main.go
+++ 06pointers/main.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to Pointers")
+	fmt.Println("Welcome to Pointers Moduke")
 
 	var ptr *int;
+	var ptr2 *string;
 
 	var x = 10;
+	var s = "patrick";
 
 	ptr = &x;
+	ptr2 = &s;
 
 	*ptr = 20;
+	*ptr2 = "Patrick";
 
-	fmt.Println("Value of variable is ",x)
+	fmt.Println("Value of variable is:",x)
+	fmt.Println("Value of variable 2 is:",s)
 
 }
